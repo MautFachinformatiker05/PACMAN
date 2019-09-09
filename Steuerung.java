5958c1a6b3e60e333adde3770674a1303b497f89
Filip Golanski <Filip Golanski@FilipGolanski>
2019-09-09 19:23:36 +0200

    Tunnel funktioniert nicht!

--- Steuerung.java
+++ Steuerung.java
@@ -19,20 +19,20 @@ public class Steuerung extends KeyAdapter {
 
 		switch (key_id){
 		case KeyEvent.VK_LEFT: 
-			System.out.println("L");
-			Game.feld.pac_richtung = 1;
+//			System.out.println("L");
+			Game.feld.pac_richtung = 0;
 			break; 
 		case KeyEvent.VK_UP: 
-			Game.feld.pac_richtung = 2; 
-			System.out.println("U");
+			Game.feld.pac_richtung = 1; 
+//			System.out.println("U");
 			break; 
 		case KeyEvent.VK_RIGHT: 
-			Game.feld.pac_richtung = 3; 
-			System.out.println("R");
+			Game.feld.pac_richtung = 2; 
+//			System.out.println("R");
 			break; 
 		case KeyEvent.VK_DOWN:
-			System.out.println("D");
-			Game.feld.pac_richtung = 0; 
+//			System.out.println("D");
+			Game.feld.pac_richtung = 3; 
 			break; 
 		}
 
